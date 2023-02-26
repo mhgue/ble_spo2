ff911995911d9022191ddab3146e67ad62d096ec
mg <mh@guenzl.de>
2023-02-26 23:18:32 +0100

    Some more plotting

--- plot_csv.sh
+++ plot_csv.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+#
+# see https://raymii.org/s/tutorials/GNUplot_tips_for_nice_looking_charts_from_a_CSV_file.html
 
 CSV="$1"
 
+# set xtics 10
+
 gnuplot -persist <<-EOFMarker
+set terminal wxt size 800,600
 set xdata time
 set timefmt "%s"
+set key autotitle columnhead
 set format x "%H:%M"
-set xtics 10
+set xlabel 'Time'
+set xtics rotate
 set datafile separator ';'
-plot "$CSV" using 2:7 title 'Motion' with linespoints, \
-     "$CSV" using 2:4 title 'Pulse' with linespoints, \
-     "$CSV" using 2:3 title 'SpO2' with linespoints
+set y2tics
+set ytics nomirror
+set ylabel "Puls/SpO2"
+set y2label "Motions"
+plot "$CSV" using 2:7 with linespoints axis x1y2, \
+     "" using 2:4 with linespoints, \
+     "" using 2:3 with linespoints
 EOFMarker
 
 #EOF
